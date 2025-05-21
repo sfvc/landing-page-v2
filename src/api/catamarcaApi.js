6befcbaaa383a8aaab1b0216a27311bcc49f4265
Nahuewe <nahuelsoriap@gmail.com>
2025-05-21 12:57:01 -0300

    Conectado con directus

--- src/api/catamarcaApi.js
+++ src/api/catamarcaApi.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const catamarcaApi = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}`
+  // baseURL: `${import.meta.env.VITE_API_URL}`
+  baseURL: `https://apis.v1.cc.gob.ar/directus`
 })
 
 catamarcaApi.interceptors.request.use(config => {
